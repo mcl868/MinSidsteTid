b233858ffb186ad1341813911a74fb7b67dd6a1c
Thomas Maltesen <atmaltesen@gmail.com>
2020-03-09 08:34:01 +0100

    Update tidtilbage.R

--- R/tidtilbage.R
+++ R/tidtilbage.R
@@ -1,3 +1,3 @@
-tidtilbage<-function(){
-	print(difftime(ISOdate(2020, 7, 31,23,59,59),Sys.time(),units = "secs"))
+tidtilbage<-function(unit = "secs"){
+	print(difftime(ISOdate(2020, 7, 31,23,59,59),Sys.time(),units = unit))
 }
